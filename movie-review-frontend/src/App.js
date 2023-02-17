aad95075aef2ce2d282d812bb066e656f2a783f3
Moyosoore Ayo-Idowu <moyosooreai@gmail.com>
2023-02-17 19:16:56 +0100

    3 - Navbar

--- movie-review-frontend/src/App.js
+++ movie-review-frontend/src/App.js
@@ -1,13 +1,46 @@
 import './App.css';
 
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { LinkContainer } from 'react-router-bootstrap';
+import { BrowserRouter, Link } from 'react-router-dom';
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import PostReview from './Components/PostReview';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">MovieReviews</a>
-      </header>
-      <main>Movies</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header className="mb-5">
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>MovieReviews</Navbar.Brand>
+              </LinkContainer>
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto w-100 justify-content-end">
+                  {/* {!userInfo && ( */}
+                  <Link className="nav-link" to="/signin">
+                    Sign In
+                  </Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Row>
+              <Col>Post a Review</Col>
+              <Col>Movies</Col>
+            </Row>
+          </Container>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ movie-review-frontend/src/Components/PostReview.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function PostReview() {
+  return <div>PostReview</div>;
+}
