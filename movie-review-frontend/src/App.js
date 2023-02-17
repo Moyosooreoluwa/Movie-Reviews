7040fbc47f2fcb202863c45e61b8135edadabde7
Moyosoore Ayo-Idowu <moyosooreai@gmail.com>
2023-02-17 17:58:12 +0100

    1 - Setup

---
+++ movie-review-frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">MovieReviews</a>
+      </header>
+      <main>Movies</main>
+    </div>
+  );
+}
+
+export default App;
